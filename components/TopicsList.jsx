e17d98cb5376553e40ed35ac66a7891ebcf168ad
sawzwe26 <sawjohnwaiyan@gmail.com>
2023-07-21 22:09:23 +0700

    pls work

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -45,7 +45,7 @@ async function TopicsList() {
     
     <>
       {topics?.map((t)=>(
-      <div className='p-4 border border-slate-3 my-3 flex justify-between gap-5 items-start'>
+      <div key={t._id} className='p-4 border border-slate-3 my-3 flex justify-between gap-5 items-start'>
         <div>
             <h2 className='font-bold text-2xl'>{t.title}</h2>
             <div>{t.description}</div>
